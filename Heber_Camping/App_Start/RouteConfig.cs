e7bba51514607d0785dbe0814d179fa1335e7e78
Lukas <luheber@tsn.at>
2020-04-01 09:29:09 +0200

    RouteConfig geändert

--- Heber_Camping/App_Start/RouteConfig.cs
+++ Heber_Camping/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Heber_Camping
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "about", id = UrlParameter.Optional }
+                defaults: new { controller = "Campingplatz", action = "infos", id = UrlParameter.Optional }
             );
         }
     }
